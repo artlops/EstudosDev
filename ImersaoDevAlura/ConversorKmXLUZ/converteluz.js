5dc672424e55119ca1c29dd792b40bd2e2623f59
artlops <arthur.lopeslima1@gmail.com>
2021-09-16 14:04:06 -0300

    Converter velocidade da luz

---
+++ ImersaoDevAlura/ConversorKmXLUZ/converteluz.js
@@ -0,0 +1,7 @@
+function ConverterLuz() {
+    var valorElemento = document.getElementById("valor");
+    var valor = valorElemento.value;
+    var valorEmKm = parseFloat(valor);
+
+    var ValorLuz = valorEmKm * 1079252848.8
+}
