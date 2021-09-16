dcd44dc1821fbbbf2446cd43e56b6e115f9d934d
artlops <arthur.lopeslima1@gmail.com>
2021-09-15 23:20:44 -0300

    Aula2

--- ImersaoDevAlura/Aula2ConversorMoeda/Aula2.js
+++ ImersaoDevAlura/Aula2ConversorMoeda/Aula2.js
@@ -6,12 +6,35 @@ function Converter() {
     var valorEmReal = valorEmDolarNumerico * 5;
   
     var elementoValorConvertido = document.getElementById("valorConvertido");
-    var valorConvertido = "O resultado em Real é R$ " + valorEmReal;
+    var valorConvertido = "O resultado em Real é R$ " + valor;
     elementoValorConvertido.innerHTML = valorConvertido;
   }
   
-  //adicionar botão converter euro
-  //converter em bitcoin
-  //comversor velocidade da luz X km
+  function ConverterEuro() {
+    var valorElementoEU = document.getElementById("valorEU");
+    var valorEU = valorElementoEU.value;
+    var valorEmDolarNumerico = parseFloat(valorEU);
+  
+    var valorEmEuro = valorEmDolarNumerico * 1.17;
+  
+    var elementoValorConvertidoEU = document.getElementById("valorConvertidoEuro");
+    var valorConvertidoEuro = "O resultado em Euro é EU$ " + valorEmEuro;
+    elementoValorConvertidoEU.innerHTML = valorConvertidoEuro;
+  }
+
+  function ConverterBitcoin() {
+    var valorElementoBitCoin = document.getElementById("valorBitCoin");
+    var valorBitCoin = valorElementoBitCoin.value;
+    var valorEmDolarNumerico = parseFloat(valorBitCoin);
+  
+    var valorEmBitCoin = valorEmDolarNumerico * 47985;
+  
+    var elementoValorConvertidoBitCoin = document.getElementById("valorConvertidoBitCoin");
+    var valorConvertidoBitCoin = "O resultado em BitCoin é $ " + valorEmBitCoin;
+    elementoValorConvertidoBitCoin.innerHTML = valorConvertidoBitCoin;
+  }
+  //adicionar botão converter euro*
+  //converter em bitcoin*
+  //conversor velocidade da luz X km
   //conversor temperatura fahrenheit celsius
   
